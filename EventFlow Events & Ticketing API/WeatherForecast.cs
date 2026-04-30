0541566d4b2ccdc87a875109131266e19e51acad
Student <Student@PGEESTEM-1>
2026-04-30 10:06:01 +0300

    Add project files.

---
+++ EventFlow Events & Ticketing API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace EventFlow_Events___Ticketing_API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
