31cf2b62b1ca41b88682781f52a4db18432bcd4c
angelanatolipetrov <angelanatolipetrov@gmail.com>
2026-05-07 10:12:21 +0300

    changes 2

---
+++ EventFlow.Domain/Entities/Venue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventFlow.Domain.Entities
+{
+    internal class Venue
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public ICollection<Event> Events { get; set; } = new List<Event>();
+    }
+}
---
+++ EventFlow.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EventFlow.Tests.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
